7557e02bcaf37f2c954857a5715fe68684571380
Mike Bridge <mbridge@classy.org>
2021-09-11 17:03:55 -0600

    add tycherion link

--- src/main/footer.tsx
+++ src/main/footer.tsx
@@ -1,6 +1,8 @@
 import {timeAgoInWords} from "./utils";
 import {metaData, summary} from "./filmData";
 import React from "react";
+import {version} from "../../package.json";
+
 
 export const Footer = () => (
     <footer>
@@ -16,7 +18,9 @@ export const Footer = () => (
             <hr/>
             <div className="text-muted">Powered by <a href="https://github.com/mikebridge/scrapeterion"
                                                       rel="noreferrer"
-                                                      target="_blank">scrapeterion {metaData.scrapeterion}</a>.
+                                                      target="_blank">scrapeterion {metaData.scrapeterion}</a> / <a href="https://github.com/mikebridge/tycherion"
+                                                                                                                    rel="noreferrer"
+                                                                                                                    target="_blank">tycherion {version}</a>.
             </div>
             <div className="text-muted font-italic"><sup>*</sup> This site is not affiliated with Criterion
                 Channel.
